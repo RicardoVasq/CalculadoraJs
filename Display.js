93bd91b5902c9e6702e63a80bea977ef1c5398b6
RicardoVasq <82993974+RicardoVasq@users.noreply.github.com>
2021-06-12 21:59:50 -0600

    El problema del script era la posicion de insercion, tiene que ir al final o utilizar defer

--- Display.js
+++ Display.js
@@ -1,18 +1,39 @@
 class Display {
-     constructor(displayValorAnterior, displayValorActual){
+     constructor(displayValorAnterior, displayValorActual) {
           this.displayValorActual = displayValorActual;
           this.displayValorAnterior = displayValorAnterior;
           this.calculador = new Calculadora();
+          this.tipoOperacion = undefined;
           this.valorActual = '';
           this.valorAnterior = '';
+          this.signos = {
+               sumar: '+',
+               dividir: '%',
+               multiplicar: 'x',
+               restar: '-',
+          }
+     }
+
+     borrar(){
+          this.valorActual = this.valorActual.toString().slice(0,-1);
+          this.imprimirElemento()
+     }
+
+     borrarTodo (){
+          this.valorActual = "";
+          this.valorAnterior = "";
+          this.tipoOperacion = undefined;
+          this.imprimirElemento();
      }
 
      agregarNumero(numero){
-          this.valorActual = numero;
+          if(numero == "." && this.valorActual.includes(".")) return
+          this.valorActual = this.valorActual.toString() + numero.toString();
           this.imprimirElemento();
      }
 
      imprimirElemento(){
      this.displayValorActual.textContent = this.valorActual;
+     this.displayValorAnterior.textContent = this.valorAnterior;
      }
 }
